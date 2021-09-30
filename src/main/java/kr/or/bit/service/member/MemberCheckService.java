390e933b658a71886f2d56acbdcceae32d6ff472
ardevnt <ardevnt@gmail.com>
2021-10-01 01:57:50 +0900

    add file

---
+++ src/main/java/kr/or/bit/service/member/MemberCheckService.java
@@ -0,0 +1,15 @@
+package kr.or.bit.service.member;
+
+import kr.or.bit.action.Action;
+import kr.or.bit.action.ActionForward;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MemberCheckService implements Action {
+
+    @Override
+    public ActionForward execute(HttpServletRequest request, HttpServletResponse response) {
+        return null;
+    }
+}
